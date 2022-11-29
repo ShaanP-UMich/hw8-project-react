9ecb812bb65a2ca676f855c762044a25535b2a6f
Shaan Parikh <shaanp@umich.edu>
2022-11-29 12:48:05 -0500

    Changed some responsive styling for Components, working on Home.js now

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,19 @@ export default class Home extends Component {
             <div>
                 {/* <div class="skip"><Link to="/#main">Skip to Main Content</Link></div> */}
 
-                <main className="container" id="main">
-                    <h1 className="title">About</h1>
-                    <p className="main-paragraph">A site showcasing beautifully built custom keyboards.</p>
+                <main className="home-page" id="main">
+                    <div className='home-section'>
+                        <h1 className="title">About</h1>
+                        <img src={require('./imgs/about_2.png')} />
+                        <p className="main-paragraph">A site showcasing beautifully built custom keyboards.</p>
+                    </div>
+                    <div className='home-section'>
+                        <h1 className="title">Why</h1>
+                        <img src={require('./imgs/why_1.png')} />
+                        <p className="main-paragraph">Whether you're an enthusiast or a newbie, anyone can build the perfect
+                            keyboard for an afforadable price. Conventional keyboards can cost a lot, which is why we're here
+                            to help you make the switch.</p>
+                    </div>
                 </main>
             </div>
         );

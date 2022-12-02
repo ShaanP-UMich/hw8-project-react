ea0e61c3cb8d7b612d1524ff1552635bef097831
Shaan Parikh <shaanp@umich.edu>
2022-12-02 15:04:22 -0500

    Added some tab indices

--- src/home.js
+++ src/home.js
@@ -33,10 +33,10 @@ export default class Home extends Component {
                         </div>
                         <p className="main-paragraph">By following these simple guides, anyone can assembled their
                             very own custom keyboard.</p>
-                        <div className='embed-video-container'>
-                            <iframe src="https://www.youtube-nocookie.com/embed/Sm1DVbyeDiI" title="How to build your FIRST custom keyboard! (ON A BUDGET)" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen aria-hidden={true}></iframe>
-                            <iframe src="https://www.youtube-nocookie.com/embed/deVaUvywGMw" title="How to Build a Mechanical Keyboard WITHOUT Soldering" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen aria-hidden={true}></iframe>
-                            <iframe src="https://www.youtube-nocookie.com/embed/9P74eCU19d0" title="I Wish I Had Known This Before I Bought My First Custom Mechanical Keyboard | Beginners Guide" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen aria-hidden={true}></iframe>
+                        <div tabIndex={0} className='embed-video-container'>
+                            <iframe tabIndex={0} src="https://www.youtube-nocookie.com/embed/Sm1DVbyeDiI" title="How to build your FIRST custom keyboard! (ON A BUDGET)" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen aria-hidden={true}></iframe>
+                            <iframe tabIndex={0} src="https://www.youtube-nocookie.com/embed/deVaUvywGMw" title="How to Build a Mechanical Keyboard WITHOUT Soldering" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen aria-hidden={true}></iframe>
+                            <iframe tabIndex={0} src="https://www.youtube-nocookie.com/embed/9P74eCU19d0" title="I Wish I Had Known This Before I Bought My First Custom Mechanical Keyboard | Beginners Guide" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen aria-hidden={true}></iframe>
                         </div>
                     </div>
                 </main>

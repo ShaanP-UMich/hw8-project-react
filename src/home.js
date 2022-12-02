2045b1e9547179c3fcb50d84363f34d2076639d5
Shaan Parikh <shaanp@umich.edu>
2022-12-02 11:07:39 -0500

    Added 'How' section and made iframes responsive

--- src/home.js
+++ src/home.js
@@ -29,6 +29,21 @@ export default class Home extends Component {
                             keyboard for an afforadable price. Conventional keyboards can cost a lot, which is why we're here
                             to help you make the switch.</p>
                     </div>
+
+                    <div className='home-section'>
+                        <div className='home-image-container'>
+                            <h1 className="title">How</h1>
+                            {/* <img src={require('./imgs/about_2.png')} /> */}
+                            <div id='how' className='home-image-banner'></div>
+                        </div>
+                        <p className="main-paragraph">By following these simple guides, anyone can assembled their
+                            very own custom keyboard.</p>
+                        <div className='embed-video-container'>
+                            <iframe src="https://www.youtube-nocookie.com/embed/Sm1DVbyeDiI" title="How to build your FIRST custom keyboard! (ON A BUDGET)" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                            <iframe src="https://www.youtube-nocookie.com/embed/deVaUvywGMw" title="How to build your FIRST custom keyboard! (ON A BUDGET)" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                            <iframe src="https://www.youtube-nocookie.com/embed/9P74eCU19d0" title="How to build your FIRST custom keyboard! (ON A BUDGET)" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        </div>
+                    </div>
                 </main>
             </div>
         );

9c18f9135f09ef4153985c6e0dba21b2c037e78c
Shaan Parikh <shaanp@umich.edu>
2022-12-06 11:44:27 -0500

    Revert "Got rid of useless imports"
    This reverts commit 7415369c40ca5443f81c2cfc7a5e6eb052f848df.

--- src/components.js
+++ src/components.js
@@ -1,6 +1,8 @@
 import './css/style.css'
 import React, { Component } from 'react';
 import ExtraInfo from './extraInfo';
+// import case_1 from './imgs/case_1.png'
+// import case_2 from './imgs/case_2.png'
 
 export default class Components extends Component {
     constructor(props) {
